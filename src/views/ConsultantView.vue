e20e539b3ab99e9b948e108008ea70c8e1429b92
Christian Toledo <christian@digitalpeople.se>
2023-03-03 15:23:06 +0100

    Add two new view pages

---
+++ src/views/ConsultantView.vue
@@ -0,0 +1,26 @@
+# Vue view page that display the details of a consultant 
+
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'ConsultantView',
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  computed: {
+  },
+})
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home-container">
+    <p>Deez nuts</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    goToShowDetails(id: string) {
+      this.$router.push({ name: 'ShowDetails', params: { showId: id } });
+    }
+  },
+  computed: {
+    showsData() {
+      return this.$store.getters.getShows;
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+@import "../assets/scss/base";
+.home-container {
+  .home-content {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+  }
+}
+@media only screen and (max-width: $tiny) {
+  .home-container {
+    .home-content {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    }
+  }
+}
+@media only screen and (min-width: $mobile) {
+  .home-container {
+    .home-content {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    }
+  }
+}
+@media only screen and (min-width: $large) {
+  .home-container {
+    padding: 50px 100px;
+    .home-content {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+    }
+  }
+}
+</style>
