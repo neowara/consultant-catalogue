c2f49d07362b96375e6748fcab86dc7c87c93f2c
Christian Toledo <christian@digitalpeople.se>
2023-03-06 12:39:13 +0100

    Add mounted to App.vue and some methods.

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,36 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { RouterView } from "vue-router";
+import { defineComponent } from "vue";
 
-export default {
+export default defineComponent ({
   name: "App",
   components: {
     RouterView
   },
-};
+  data() {
+    return {
+      availableConsultants: [] as Array<object>,
+      selectedConsultant: Number,
+    };
+  },
+  methods: {
+    async getConsultants(): Promise<void> {
+      await this.$store.dispatch("getConsultants", this.selectedConsultant);
+      this.availableConsultants = this.$store.state.consultants;
+    },
+  },
+  computed: {
+    consultantData(): object {
+      return this.$store.getters.consultantData;
+    }
+  },
+  mounted() {
+    this.getConsultants();
+  },
+});
 </script>
 
 <style lang="scss">
