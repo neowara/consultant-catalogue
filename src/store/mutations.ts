a38527ab8b26badec5de85424f99c68a6f938e38
Christian Toledo <mugidota@hotmail.com>
2023-04-06 13:46:36 +0200

    Delete unnecessary files and some formatting

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -23,10 +23,11 @@ const mutations: MutationTree<state> = {
   toggleMenu(state: state, payload) {
     state.menuOpen = payload;
   },
-  // This code checks if there is a consultant in local storage.
-  // If there is, then the state will be set to the data that is in local storage.
-  // If there is no consultant in local storage, then the state will be set to the data that is passed into the function.
-  // then the data is set to local storage as well.
+  
+// This code checks if there is a consultant in local storage. 
+// If there is, then the state will be set to the data that is in local storage. 
+// If there is no consultant in local storage, then the state will be set to the data that is passed into the function.
+// then the data is set to local storage as well.
   setConsultants(state: state, data: Array<Consultant>) {
     if (localStorage.getItem("consultants")) {
       state.consultants = JSON.parse(
