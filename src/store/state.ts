2f442f3c7a383043f61b0b7bdc1627d9c1966206
Christian Toledo <mugidota@hotmail.com>
2023-04-14 12:11:49 +0200

    Add filter interface and filter object to Vuex

--- src/store/state.ts
+++ src/store/state.ts
@@ -23,13 +23,25 @@ export interface Consultant {
   };
 }
 
+export interface Filter {
+  name: string;
+  key: string;
+  btnType: string;
+  options: {
+    value: string;
+    label: string;
+  }[];
+}
+
 export interface state {
   APIAdress: string,
   active: object,
   menuOpen: boolean,
   consultants: Array<Consultant>,
   keyword: string,
+  filters: Array<Filter>,
 }
+
 
 export const state: state = {
   APIAdress: "https://available-consultantsapi.azurewebsites.net/api/AvailableConsultants?",
@@ -37,4 +49,58 @@ export const state: state = {
   menuOpen: false,
   consultants: [],
   keyword: "",
+  filters: [
+    {
+      name: 'Location',
+      key: 'location',
+      btnType: 'clickable',
+      options: [
+        { value: 'new-york', label: 'New York' },
+        { value: 'san-francisco', label: 'San Francisco' },
+        { value: 'london', label: 'London' },
+        { value: 'paris', label: 'Paris' },
+      ],
+    },
+    {
+      name: 'Business Area',
+      key: 'businessArea',
+      btnType: 'clickable',
+      options: [
+        { value: 'technology', label: 'Technology' },
+        { value: 'finance', label: 'Finance' },
+        { value: 'marketing', label: 'Marketing' },
+        { value: 'sales', label: 'Sales' },
+      ],
+    },
+    {
+      name: 'Working Titles',
+      key: 'workingTitles',
+      btnType: 'clickable',
+      options: [
+        { value: 'consultant', label: 'Consultant' },
+        { value: 'senior-consultant', label: 'Senior Consultant' },
+        { value: 'manager', label: 'Manager' },
+        { value: 'senior-manager', label: 'Senior Manager' },
+      ],
+    },
+    {
+      name: 'Can Travel',
+      key: 'canTravel',
+      btnType: 'checkbox',
+      options: [
+        { value: 'true', label: 'Yes' },
+        { value: 'false', label: 'No' },
+      ],
+    },
+    {
+      name: 'Available Type',
+      key: 'availableType',
+      btnType: 'clickable',
+      options: [
+        { value: 'full-time', label: 'Full-time' },
+        { value: 'contract', label: 'Contract' },
+        { value: 'freelance', label: 'Freelance' },
+      ],
+    },
+  ]
 }
